dbb3f9457b4adefdffae613bef6bfe3ef787fddc
Hyunsik Choi <hyunsik@apache.org>
2015-09-22 08:50:52 -0700

    Separate jni module and add more examples.

--- src/main/rust/src/lib.rs
+++ src/main/rust/src/lib.rs
@@ -6,13 +6,13 @@
 
 extern crate libc;
 
-extern crate jni_native;
+extern crate jni;
 
 use libc::c_void;
-use std::ffi::{CStr, CString};
 use std::ptr;
 
-use jni_native::*;
+use jni::native::*;
+use jni::helper::*;
 
 #[no_mangle]
 pub extern fn Java_org_github_hyunsik_NativeInvoke_procedure(jre: *mut JNIEnv,
@@ -29,9 +29,7 @@ pub extern fn Java_org_github_hyunsik_NativeInvoke_stringArg(jre: *mut JNIEnv,
     class: *const c_void, name: jstring) {
   unsafe {
       let string = ((**jre).GetStringUTFChars)(jre, name, ptr::null_mut());
-      let c_str: &CStr = CStr::from_ptr(string);
-      let bytes = c_str.to_bytes();
-      println!("{}", std::str::from_utf8(bytes).unwrap());
+      println!("{}", chars_to_str(string));
 
        ((**jre).ReleaseStringUTFChars)(jre, name, string);
   }
@@ -41,7 +39,6 @@ pub extern fn Java_org_github_hyunsik_NativeInvoke_stringArg(jre: *mut JNIEnv,
 pub extern fn Java_org_github_hyunsik_NativeInvoke_returnString(jre: *mut JNIEnv,
     class: *const c_void) -> jstring {
   unsafe {
-      let c_str = CString::new("jni native".as_bytes()).unwrap();
-      return ((**jre).NewStringUTF)(jre, c_str.as_ptr());
+      return str_to_jstring(jre, "jni native");
   }
 }
